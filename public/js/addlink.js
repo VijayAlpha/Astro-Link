39044047c9136f04144cc6c3fee7966784df9ef5
Vijay M <vijaymurugan177@gmail.com>
2021-11-24 20:31:39 +0530

    just fixed the route address

--- public/js/addlink.js
+++ public/js/addlink.js
@@ -2,7 +2,7 @@ export const addLink = async (data) => {
     try {
       const res = await axios({
         method: 'POST',
-        url: 'http://127.0.0.1:3000/api/v1/link/addLink',
+        url: '/api/v1/link/addLink',
         data
       });
   
