b199303ec7e0c971f029edac8f15009f90d7bee7
VijayAlpha <vijaymurugan177@gmail.com>
2021-11-25 22:46:03 +0530

    added link option

---
+++ public/js/style.js
@@ -0,0 +1,20 @@
+// Style Function
+const mediaLinkBox = document.querySelector('.media-link');
+const mediaLinkOption = document.querySelector('.link-option');
+const mediaLinkOption_open = document.querySelector('#link-option--open');
+const mediaLinkOption_linkDelete= document.querySelector('#link-option--delete');
+
+mediaLinkBox.addEventListener('click', function(e) {
+
+  if(e.target.parentNode.className == "media-link__box"){
+    mediaLinkOption.style.display = "block";
+
+    mediaLinkOption_open.href = e.target.parentNode.dataset.link;
+    mediaLinkOption_linkDelete.href = e.target.parentNode.id;
+  }
+  
+});
+
+mediaLinkOption.addEventListener('click' , ()=>{
+  mediaLinkOption.style.display = "none";
+})
