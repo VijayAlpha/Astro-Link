830afd59877371be469d90345419aa87ccdf05e5
VijayAlpha <vijaymurugan177@gmail.com>
2021-11-29 20:49:14 +0530

    added updateMyPassword view with error

--- public/js/index.js
+++ public/js/index.js
@@ -1,60 +1,80 @@
-import { login , signup } from './login.js';
-import { addLink , deleteLink } from './link.js';
+import { login, signup } from './login.js';
+import { addLink, deleteLink } from './link.js';
 import { userSettings } from './settings.js';
 
 const loginForm = document.querySelector('.form--login');
 const signupForm = document.querySelector('.form--signup');
 const addLinkForm = document.querySelector('.form--add-link');
 const userSettingsForm = document.querySelector('.form--user-settings');
-const mediaLinkOption_linkDelete= document.querySelector('#link-option--delete');
+const userPasswordForm = document.querySelector('.form--user-password');
+const mediaLinkOption_linkDelete = document.querySelector(
+  '#link-option--delete'
+);
 
-if(mediaLinkOption_linkDelete){
-  mediaLinkOption_linkDelete.addEventListener('click' , () => {
+if (mediaLinkOption_linkDelete) {
+  mediaLinkOption_linkDelete.addEventListener('click', () => {
     deleteLink(mediaLinkOption_linkDelete.dataset.linkId);
-  })
+  });
 }
 
-if(userSettingsForm){
+if (userSettingsForm) {
   userSettingsForm.addEventListener('submit', e => {
     e.preventDefault();
+    document.querySelector('.btn--save-data').textContent = 'Updating...';
+
     const form = new FormData();
     form.append('name', document.getElementById('name').value);
     form.append('userName', document.getElementById('user-name').value);
     form.append('userBio', document.getElementById('user-bio').value);
     form.append('email', document.getElementById('email').value);
 
-    const profilePic = document.getElementById('profile-image').files[0]
-    const bannerPic = document.getElementById('banner-image').files[0]
-    if (profilePic)
-     form.append('avatar', profilePic);
-    if (bannerPic)
-     form.append('banner' , bannerPic);
+    const profilePic = document.getElementById('profile-image').files[0];
+    const bannerPic = document.getElementById('banner-image').files[0];
+    if (profilePic) form.append('avatar', profilePic);
+    if (bannerPic) form.append('banner', bannerPic);
 
     // const name = document.getElementById('name').value;
     // const userName = document.getElementById('user-name').value;
     // const userBio = document.getElementById('user-bio').value;
     // const email = document.getElementById('email').value;
 
-    userSettings(form);
+    userSettings(form, 'data');
   });
 }
 
-if(addLinkForm){
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await userSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+
+if (addLinkForm) {
   addLinkForm.addEventListener('submit', e => {
     e.preventDefault();
 
     const link = document.getElementById('link').value;
     const linkName = document.getElementById('link-name').value;
     const linkDescription = document.getElementById('link-description').value;
-    const photo = document.getElementById('link-image').files[0]
-    
+    const photo = document.getElementById('link-image').files[0];
+
     const form = new FormData();
     form.append('link', link);
-    form.append('linkName', linkName );
-    if(linkDescription)
-      form.append('linkDescription', linkDescription);
-    if(photo)
-      form.append('photo', photo);
+    form.append('linkName', linkName);
+    if (linkDescription) form.append('linkDescription', linkDescription);
+    if (photo) form.append('photo', photo);
 
     // const linkName = document.getElementById('link-name').value;
     // const linkDescription = document.getElementById('link-description').value;
@@ -79,6 +99,5 @@ if (signupForm)
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('confirm-password').value;
-    signup(name , userName , email, password , passwordConfirm);
+    signup(name, userName, email, password, passwordConfirm);
   });
-
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,11 +1,18 @@
-export const userSettings = async (data) => {
+export const userSettings = async (data , type ) => {
     try {
+     
+      const url =
+      type === 'password'
+        ? '/api/v1/user/updateMyPassword'
+        : '/api/v1/user/updateMe';
+     
       const res = await axios({
         method: 'PATCH',
-        url: '/api/v1/user/updateMe',
+        url,
         data
       });
   
+      // afterward change to show alert as data saved
       if (res.data.status === 'success') {
         window.setTimeout(() => {
           location.assign('/me');
