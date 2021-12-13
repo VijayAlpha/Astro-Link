4b6b5b6c376b7b61b609bf7d063bc1d2134d8a46
Vijay M <vijaymurugan177@gmail.com>
2021-12-13 18:39:57 +0530

    added add social acc settings FE

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,24 +1,29 @@
-export const userSettings = async (data , type ) => {
-    try {
-     
-      const url =
-      type === 'password'
-        ? '/api/v1/user/updateMyPassword'
-        : '/api/v1/user/updateMe';
-     
-      const res = await axios({
-        method: 'PATCH',
-        url,
-        data
-      });
-  
-      // afterward change to show alert as data saved
-      if (res.data.status === 'success') {
-        window.setTimeout(() => {
-          location.assign('/me');
-        }, 500);
+export const userSettings = async (data, type) => {
+  try {
+    const url =
+      type === "password"
+        ? "/api/v1/user/updateMyPassword"
+        : "/api/v1/user/updateMe";
+
+    if(type === "social"){
+      data = {
+        socialLinks: data
       }
-    } catch (err) {
-      console.log(err.response)
     }
-  };
+
+    const res = await axios({
+      method: "PATCH",
+      url,
+      data,
+    });
+
+    //afterward change to show alert as data saved
+    if (res.data.status === "success") {
+      window.setTimeout(() => {
+        location.assign("/me");
+      }, 500);
+    }
+  } catch (err) {
+    console.log(err.response);
+  }
+};
