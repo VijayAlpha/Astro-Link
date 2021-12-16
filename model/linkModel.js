2b2299b40caa94af2994661230f09286cca4b87e
VijayAlpha <vijaymurugan177@gmail.com>
2021-12-16 15:01:00 +0530

    Added edit link controller

--- model/linkModel.js
+++ model/linkModel.js
@@ -1,28 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import validator from 'validator';
 
-const linkSchema =  new mongoose.Schema({
-    linkName: {
-        type: String,
-        required: [true , 'Please give a name to your link.']
-    },
-    link: {
-        type: String,
-        required: [true , 'Please provide a link.'],
-        validate: [validator.isURL, 'Please provide a valid URL']
-    },
-    linkDescription: {
-        type: String
-    },
-    photo: String,
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: [true, 'Link must belong to a user']
-      }
+const linkSchema = new mongoose.Schema({
+  linkName: {
+    type: String,
+    required: [true, 'Please give a name to your link.'],
+  },
+  link: {
+    type: String,
+    required: [true, 'Please provide a link.'],
+    validate: [validator.isURL, 'Please provide a valid URL'],
+  },
+  linkDescription: {
+    type: String,
+  },
+  photo: String,
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Link must belong to a user'],
+  },
 });
 
 const Link = mongoose.model('Link', linkSchema);
 
 export default Link;
-  
