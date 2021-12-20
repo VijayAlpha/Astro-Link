44b6b9dbbc16a0f31b8159277be967813fb75139
VijayAlpha <vijaymurugan177@gmail.com>
2021-12-20 12:34:18 +0530

    Added image preview in account seetings

--- public/js/file-preview.js
+++ public/js/file-preview.js
@@ -1,26 +1,54 @@
 import { updateLink } from './link.js';
 
+const profileImageInput = document.getElementById('profile-image');
+const profileImage = document.getElementById('profile-img');
+
+const bannerImageInput = document.getElementById('banner-image');
+const bannerImage = document.getElementById('banner-img');
+
 const linkImageInput = document.getElementById('link-image');
 const linkImage = document.getElementById('input--image-preview');
 const linkImageDelete = document.getElementById('link-image--delete');
 const editLinkForm = document.querySelector('.form--edit-link');
 
-linkImageInput.addEventListener('change', function showPreview(event) {
-  if (event.target.files.length > 0) {
-    var src = URL.createObjectURL(event.target.files[0]);
-    var preview = linkImage;
-    preview.src = src;
-    preview.style.display = 'block';
-  }
-});
+if (profileImageInput) {
+  profileImageInput.addEventListener('change', function showPreview(event) {
+    if (event.target.files.length > 0) {
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = profileImage;
+      preview.src = src;
+    }
+  });
+}
 
-linkImageDelete.addEventListener('click', e => {
-  e.preventDefault();
-  linkImage.src = '../img/link-image/add-image.svg';
-  const photo = null;
+if (bannerImage) {
+  bannerImageInput.addEventListener('change', function showPreview(event) {
+    if (event.target.files.length > 0) {
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = bannerImage;
+      preview.src = src;
+    }
+  });
+}
 
-  const form = new FormData();
-  form.append('photo', photo);
+if (linkImageInput) {
+  linkImageInput.addEventListener('change', function showPreview(event) {
+    if (event.target.files.length > 0) {
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = linkImage;
+      preview.src = src;
+    }
+  });
+}
+if (linkImageDelete) {
+  linkImageDelete.addEventListener('click', e => {
+    e.preventDefault();
+    linkImage.src = '../img/link-image/add-image.svg';
+    const photo = undefined;
 
-  updateLink(editLinkForm.id, form);
-});
+    const form = new FormData();
+    form.append('photo', photo);
+
+    updateLink(editLinkForm.id, form);
+  });
+}
