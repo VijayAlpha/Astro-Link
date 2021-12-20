f9bff6aaa1a0064c47dd2cf904139606eee74111
VijayAlpha <vijaymurugan177@gmail.com>
2021-12-20 11:45:01 +0530

    changes in user name update

--- model/userModel.js
+++ model/userModel.js
@@ -1,27 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import validator from 'validator';
-import bcrypt  from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
-const userSchema =  new mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, 'Please provide username'],
-    unique: true,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, 'Please provide username'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     name: {
       type: String,
-      required: [true, 'Please tell us your name!']
+      required: [true, 'Please tell us your name!'],
     },
     email: {
       type: String,
       required: [true, 'Please provide your email'],
       unique: true,
       lowercase: true,
-      validate: [validator.isEmail, 'Please provide a valid email']
+      validate: [validator.isEmail, 'Please provide a valid email'],
     },
     avatar: {
       type: String,
-      default: 'default.jpg'
+      default: 'default.jpg',
     },
     banner: {
       type: String,
@@ -30,21 +33,21 @@ const userSchema =  new mongoose.Schema({
       type: String,
       required: [true, 'Please provide a password'],
       minlength: 8,
-      select: false
+      select: false,
     },
     passwordConfirm: {
       type: String,
       required: [true, 'Please confirm your password'],
       validate: {
         // This only works on CREATE and SAVE!!!
-        validator: function(el) {
+        validator: function (el) {
           return el === this.password;
         },
-        message: 'Passwords are not the same!'
-      }
+        message: 'Passwords are not the same!',
+      },
     },
     userBio: {
-      type: String
+      type: String,
     },
     // socialLinks:[
     //   {
@@ -61,11 +64,14 @@ const userSchema =  new mongoose.Schema({
     socialLinks: {
       phone: {
         type: String,
-        validate: [validator.isMobilePhone, 'Please provide a valid phone number']
+        validate: [
+          validator.isMobilePhone,
+          'Please provide a valid phone number',
+        ],
       },
       email: {
         type: String,
-        validate: [validator.isEmail, 'Please provide a valid email']
+        validate: [validator.isEmail, 'Please provide a valid email'],
       },
       instagram: {
         type: String,
@@ -81,41 +87,42 @@ const userSchema =  new mongoose.Schema({
       },
       facebook: {
         type: String,
-        validate: [validator.isURL, 'Please provide a valid URL']
+        validate: [validator.isURL, 'Please provide a valid URL'],
       },
       linkedIn: {
         type: String,
-        validate: [validator.isURL, 'Please provide a valid URL']
+        validate: [validator.isURL, 'Please provide a valid URL'],
       },
       pinterest: {
         type: String,
-        validate: [validator.isURL, 'Please provide a valid URL']
+        validate: [validator.isURL, 'Please provide a valid URL'],
       },
       youtube: {
         type: String,
-        validate: [validator.isURL, 'Please provide a valid URL']
+        validate: [validator.isURL, 'Please provide a valid URL'],
       },
       whatsapp: {
         type: String,
-        validate: [validator.isMobilePhone, 'Please provide a valid phone number']
-      }
-    }
+        validate: [
+          validator.isMobilePhone,
+          'Please provide a valid phone number',
+        ],
+      },
+    },
   },
   {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
   }
-  );
+);
 
+userSchema.virtual('links', {
+  ref: 'Link',
+  foreignField: 'user',
+  localField: '_id',
+});
 
-  userSchema.virtual('links', {
-    ref: 'Link',
-    foreignField: 'user',
-    localField: '_id'
-  });
-
-
-  // this to save the user password in encrypted formate.(not to expoler by others)
+// this to save the user password in encrypted formate.(not to expoler by others)
 userSchema.pre('save', async function (next) {
   // if password is not modified
   if (!this.isModified('password')) return next();
